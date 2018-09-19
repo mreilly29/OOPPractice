6e4394731725153e00f2e5debd494adf083e71b8
mreilly29 <mreilly18@jcu.edu>
2018-09-19 13:19:25 -0400

    initial commit

---
+++ OOPPractice/Heroes.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Heroes
+    {
+        //FIELDS
+        private int strength = 100;
+        private bool cape;
+        private int bikeSpeed = 50;
+        private int bootSize;
+        private string name;
+
+        //properties
+        public int Strength
+        {
+            get { return this.strength; }
+            set { this.strength = value; }
+        }
+       
+        public bool Cape
+        {
+            get { return this.cape; }
+            set { this.cape = value; }
+        }
+
+        public int BikeSpeed
+        {
+            get { return this.bikeSpeed; }
+            set { this.bikeSpeed = value; }
+        }
+
+        public int BootSize
+        {
+            get { return this.bootSize; }
+            set { this.bootSize = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        
+        //CONSTRUCTORS
+        public Heroes()
+        {
+
+        }
+
+        public Heroes(int strong, bool cape)
+        {
+            this.Strength = strong;
+            this.Cape = cape;
+        }
+
+        public Heroes(string person)
+        {
+            this.Name = person;            
+        }        
+       
+        //METHODS
+        public void GetHealth()
+        {
+            Strength = Strength - 10;
+            Console.WriteLine("Stop checking your health! Strength minus 10.");
+        }
+
+        public void GetHelp()
+        {
+            Console.WriteLine("Now {0}.. I don't mean to be rude, but have you slacked with your classmates?", Name);
+            if(Console.ReadLine().ToLower() == "yes")
+            {
+                BikeSpeed += 10;
+                Console.WriteLine("Cool beans, you listened. Please Accept these 10 points for your bike speed!");
+            }
+            else
+            {
+                BikeSpeed-=20;
+                Strength -= 50;
+                Console.WriteLine("You're not Gucci. SLACK YOUR CLASSMATES FIRST.");
+                Console.WriteLine("Bike speed minus 20. Strength minus 50.");
+            }
+        }
+
+        public void IsCool (bool hasCape)
+        {
+            if (hasCape == true)
+            {
+                Strength -= 5;
+                Console.WriteLine("Fool! A cape can kill you. Strength minus 5.");
+            }
+            else
+            {
+                Strength += 15;
+                Console.WriteLine("Now your a smart hero. Strength plus 15.");
+            }
+        }
+
+        public void EatSpinach(bool healthy)
+        {
+            Strength += 7;
+        }
+        
+        
+
+    }
+}
---
+++ OOPPractice/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////USING DOG CLASS
+            //Dog woofy = new Dog();
+            //Dog spot = new Dog();
+            //Dog woofy = new Dog(7, 8.5, 33.2, 190.1, "Woofy");
+            //Dog spot = new Dog(15, 10.5, 100.9, 250.3, "Spot");
+            //Console.WriteLine("woofy's hair length is: {0}", woofy.HairLength);
+            //Console.WriteLine("spot's hair length is: {0}", spot.HairLength);
+            //woofy.HairLength = 4;
+            //spot.HairLength = 6;
+            //woofy.GetSpeed(7);
+            //spot.GetSpeed(11);
+            //Console.WriteLine("woofy's hair length is: {0}", woofy.HairLength);
+            //Console.WriteLine("spot's hair length is: {0}", spot.HairLength);
+            //Console.WriteLine("woofy is running {0}", woofy.GetSpeed(7));
+
+            //USING HEROES CLASS
+            Heroes maggie = new Heroes("Maggie");
+
+            Console.WriteLine("Hello {0}. Right now your strength is {1} and your bike speed is {2}.", maggie.Name, maggie.Strength, maggie.BikeSpeed);
+
+            Console.WriteLine("Would you like to check your health {0}?", maggie.Name);
+            if (Console.ReadLine().ToLower() == "yes")
+            {
+                maggie.GetHealth();
+            }
+           
+
+            Console.WriteLine("Do you have a cape {0}?", maggie.Name);
+            if (Console.ReadLine().ToLower() == "yes")
+            {
+                maggie.IsCool(true);
+            }
+            else
+            {
+                maggie.IsCool(false);
+            }
+
+            Console.WriteLine("Are you in danger {0}?", maggie.Name);
+            if (Console.ReadLine().ToLower() == "yes")
+            {
+                maggie.GetHelp();
+            }
+
+            Console.WriteLine("Your strength is: {0}", maggie.Strength);
+            Console.WriteLine("Your bike speed is: {0}", maggie.BikeSpeed);
+
+
+        }//Main
+    }//class Program
+}//namespace OOPPractice
